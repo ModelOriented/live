57b00c62964cc3b132373f3104a55d4efc78be10
Mateusz Staniak <mateusz.staniak@math.uni.wroc.pl>
2017-08-11 16:57:32 +0200

    package docs

---
+++ R/live.R
@@ -0,0 +1,18 @@
+#' live: visualizing interpretable models to explain black box models.
+#'
+#' This package aims to help locally fit and visualize interpretable models as in LIME methodology.
+#' Interface provided by mlr package is used. Tools are provided to create a simulated dataset of 
+#' similar observations, fit a chosen white box models (GLM and CART in particular) and visualize
+#' them. 
+#'
+#' @section Important function:
+#' \code{\link{simulateSimilar}} generates a dataset that will be used for local eploration.
+#' \code{\link{trainWhiteBox}} fits a chosen model to simulated dataset.
+#' \code{\link{plotWhiteBOx}} visualizes fitted model.
+#'
+#' @section Example datasets:
+#'
+#' 
+#' @docType package
+#' @name live
+NULL

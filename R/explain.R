ba5fa22db54b13e7d3b9f6f6e2121061bf01f123
Mateusz Staniak <mateusz.staniak@math.uni.wroc.pl>
2017-11-26 23:20:04 +0100

    simulate_similar -> sample_locally, train_white_box -> fit_explanation

--- R/explain.R
+++ R/explain.R
@@ -1,3 +1,42 @@
+#' Fit white box model to the simulated data.
+#' 
+#' @param live_object List return by simulate_similar function. 
+#' @param white_box String, learner name recognized by mlr package.
+#' @param selection If TRUE, variable selection based on AIC will be performed.
+#' @param maximum_depth maximum depth of a tree (when decision tree is used).
+#'
+#' @return mlr object returned by train function.
+#' 
+#' @export
+#' 
+
+
+fit_explanation <- function(live_object, white_box, selection = FALSE, maximum_depth = 0) {
+  if(n_distinct(live_object$data[[live_object$target]]) == 1) 
+    stop("All predicted values were equal.")
+  
+  if(selection) {
+    explained_var_col <- which(colnames(live_object$data) == live_object$target)
+    selected_vars <- selectModel(as.data.frame(live_object$data[, -explained_var_col]), 
+                                 unlist(live_object$data[, explained_var_col]), crit = aic)
+  } else {
+    selected_vars <- colnames(live_object$data)
+  }
+  
+  mlr_task <- create_task(white_box, 
+                          live_object$data[, unique(c(selected_vars, live_object$target))],
+                          live_object$target)
+  
+  if(grepl("regr", white_box)) {
+    lrn <- mlr::makeLearner(white_box)
+  } else {
+    lrn <- mlr::makeLearner(white_box, max_depth = maximum_depth)
+  }
+  
+  mlr::train(lrn, mlr_task)
+}  
+
+
 #' Draw a forest plot with proper annotations.
 #' 
 #' @param coefficients Matrix of regression coefficients (from summary).
--- R/explore.R
+++ R/explore.R
@@ -89,7 +89,7 @@ create_task <- function(model, dataset, target_var) {
 }
 
 
-#' Generate dataset for white box model based on black box model.
+#' Generate dataset for local exploration.
 #'
 #' @param data Data frame from which new dataset will be simulated.
 #' @param explained_instance One row data frame with the same variables 
@@ -110,7 +110,7 @@ create_task <- function(model, dataset, target_var) {
 #' @export
 #'
 
-simulate_similar <- function(data, explained_instance, explained_var, black_box,  
+sample_locally <- function(data, explained_instance, explained_var, black_box,  
                             size, standardise = FALSE,
                             predict_function = predict, ...) {
   similar <- generate_neighbourhood(data, explained_instance, size)
@@ -132,42 +132,3 @@ simulate_similar <- function(data, explained_instance, explained_var, black_box,
   
   list(data = similar, target = explained_var, black_box_model = black_box)
 }
-
-
-#' Fit white box model to the simulated data.
-#' 
-#' @param live_object List return by simulate_similar function. 
-#' @param white_box String, learner name recognized by mlr package.
-#' @param selection If TRUE, variable selection based on AIC will be performed.
-#' @param maximum_depth maximum depth of a tree (when decision tree is used).
-#'
-#' @return mlr object returned by train function.
-#' 
-#' @export
-#' 
-
-
-train_white_box <- function(live_object, white_box, selection = FALSE, maximum_depth = 0) {
-  if(n_distinct(live_object$data[[live_object$target]]) == 1) 
-    stop("All predicted values were equal.")
-  
-  if(selection) {
-    explained_var_col <- which(colnames(live_object$data) == live_object$target)
-    selected_vars <- selectModel(as.data.frame(live_object$data[, -explained_var_col]), 
-                                      unlist(live_object$data[, explained_var_col]), crit = aic)
-  } else {
-    selected_vars <- colnames(live_object$data)
-  }
-  
-  mlr_task <- create_task(white_box, 
-                          live_object$data[, unique(c(selected_vars, live_object$target))],
-                          live_object$target)
-  
-  if(grepl("regr", white_box)) {
-    lrn <- mlr::makeLearner(white_box)
-  } else {
-    lrn <- mlr::makeLearner(white_box, max_depth = maximum_depth)
-  }
-  
-  mlr::train(lrn, mlr_task)
-}   

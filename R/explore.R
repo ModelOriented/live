439377a4b0f984364b2b31d37ede53d48df9c10d
Mateusz Staniak <mateusz.staniak@math.uni.wroc.pl>
2017-11-28 00:34:01 +0100

    function extracted, code cleaned

--- R/explore.R
+++ R/explore.R
@@ -1,4 +1,4 @@
-#' Replace one element of a vector with a random element from given data frame.
+#' Replace one element of each row with a random element from given data frame.
 #' 
 #' @param chosen_vars Columns to sample from [as positions/numbers].
 #' @param data Data frame to sample from.
@@ -7,31 +7,13 @@
 #' @return data.frame
 #' 
 
-replace_items <- function(chosen_vars, data, target) {
+replace_items_by_rows <- function(chosen_vars, data, target) {
   lapply(seq_along(chosen_vars), function(x) {
     row <- target[x, ]
     row[1, chosen_vars[x]] <- sample(unlist(data[, chosen_vars[x]]), 1)
     row
   }) %>%
     dplyr::bind_rows()
-}
-
-#' Change value of one variable all rows.
-#'
-#' @param data Data frame from which observations will be generated.
-#' @param new_data Data frame created by generate_neighbourhood function.
-#' @param steps Number of variables to change.
-#' 
-#' @return data.frame
-#' 
-
-walk_through_vars <- function(data, new_data, steps) {
-  if(steps == ncol(new_data)) {
-    replace_items(1:ncol(data), data, new_data)
-  } else {
-    chosen_vars <- sort(sample(ncol(new_data), steps))
-    replace_items(chosen_vars, data, new_data)
-  }
 }
 
 
@@ -51,16 +33,18 @@ generate_neighbourhood <- function(data, explained_instance, size) {
   neighbourhood <- dplyr::bind_rows(lapply(1:size, function(x) explained_instance))
   
   if(size <= dimension) {
-    neighbourhood <- walk_through_vars(data, neighbourhood, size)
+    neighbourhood <- replace_items_by_rows(sort(sample(ncol(neighbourhood), size)), 
+                                           data, neighbourhood)
   } else {
     k = size %/% dimension
     r = size %% dimension
     separate <- c(rep(1:k, each = dimension), rep(k + 1, r))
     divided <- split(neighbourhood, separate)
     divided[1:k] <- lapply(divided[1:k], function(x)
-      walk_through_vars(data, x, dimension))
+                           replace_items_by_rows(sort(sample(ncol(x), dimension)), 
+                                                 data, x))
     if(r > 0) {
-      divided[[k + 1]] <- walk_through_vars(data, divided[[k + 1]], r)
+      divided[[k + 1]] <- replace_items_by_rows(sort(sample(ncol(divided[[k + 1]]), r)),                                         data, divided[[k + 1]])
     }
     neighbourhood <- dplyr::bind_rows(divided)
   }
@@ -88,6 +72,34 @@ create_task <- function(model, dataset, target_var) {
   }
 }
 
+#' Add predictions to generated dataset.
+#' 
+#' @param black_box String with mlr signature of a learner or a model with predict interface.
+#' @param explained_var Name of a column with the variable to be predicted.
+#' @param similar Dataset created for local exploration.
+#' @param predict_function Either a "predict" function that returns a vector of the
+#'        same type as response or custom function that takes a model as a first argument,
+#'        new data used to calculate predictions as a second argument called "newdata"
+#'        and returns a vector of the same type as respone. 
+#'        Will be used only if a model object was provided in the black_box argument.
+#' @param ... Additional parameters to be passed to predict function.
+#' 
+#' @return Vector of model predictions.
+#' 
+
+give_predictions <- function(black_box, explained_var, similar, predict_function, ...){
+  if(is.character(black_box)) {  
+    mlr_task <- create_task(black_box, as.data.frame(data), explained_var)
+    pred <- mlr::makeLearner(black_box) %>% 
+      mlr::train(mlr_task) %>%
+      predict(newdata = as.data.frame(similar))
+    pred[["data"]][["response"]]
+  } else {
+    predict_function(black_box, 
+                     newdata = similar, ...)
+  }
+}
+
 
 #' Generate dataset for local exploration.
 #'
@@ -103,7 +115,7 @@ create_task <- function(model, dataset, target_var) {
 #'        new data used to calculate predictions as a second argument called "newdata"
 #'        and returns a vector of the same type as respone. 
 #'        Will be used only if a model object was provided in the black_box argument.
-#' @param ... Additional parameters to be passed to makeRegrTask function.
+#' @param ... Additional parameters to be passed to predict function.
 #'
 #' @return list
 #' 
@@ -119,16 +131,7 @@ sample_locally <- function(data, explained_instance, explained_var, black_box,
       dplyr::mutate_if(is.numeric, function(x) as.vector(scale(x)))
   }
   
-  if(is.character(black_box)) {  
-    mlr_task <- create_task(black_box, as.data.frame(data), explained_var)
-    pred <- mlr::makeLearner(black_box) %>% 
-      mlr::train(mlr_task) %>%
-      predict(newdata = as.data.frame(similar))
-    similar[[explained_var]] <- pred[["data"]][["response"]]
-  } else {
-    similar[[explained_var]] <- predict_function(black_box, 
-						  newdata = similar, ...)
-  }
+  similar[[explained_var]] <- give_predictions(black_box, explained_var, similar, predict_function, ...)
   
   list(data = similar, target = explained_var, black_box_model = black_box)
 }

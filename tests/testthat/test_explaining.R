ca1cb7b64be22227dd04901e33bda2ae1921fd4d
Mateusz Staniak <mateusz.staniak@math.uni.wroc.pl>
2017-11-30 00:16:35 +0100

    tests

---
+++ tests/testthat/test_explaining.R
@@ -0,0 +1,21 @@
+context("Fitting and plotting explanations")
+
+set.seed(1)
+X <- tibble::as_tibble(MASS::mvrnorm(50, rep(0, 10), diag(1, 10)))
+local <- live::sample_locally(data = X,
+                              explained_instance = X[3, ], 
+                              explained_var = "V1",
+                              size = 50)
+local <- live::add_predictions(X, local, "regr.lm")
+local_explained <- live::fit_explanation(local, "regr.lm")
+
+test_that("White box model is fitted correctly", {
+  expect_is(local_explained, "WrappedModel")
+  expect_is(mlr::getLearnerModel(local_explained), "lm")
+})
+
+test_that("Plots are created without problems", {
+  expect_output(live::plot_explanation(local_explained, X[3, ]), regexp = NA)
+  expect_output(live::plot_explanation(local_explained))
+})
+
--- tests/testthat/test_simulating.R
+++ tests/testthat/test_simulating.R
@@ -40,12 +40,26 @@ test_that("Not too many changes are made", {
   
 })
 
-# test_that("Factors are treated correctly", {
-#   
-# })
-# 
-# test_that("Missing data are detected and treated properly", {
-# 
-# })
+test_that("Missing data are detected warning is given", {
+  Y <- X
+  Y[1, 1] <- NA
+  expect_warning(check_for_na(Y, Y[1, ]))
+  expect_warning(check_for_na(X, X[4, ]), regexp = NA)
+})
 
-
+test_that("Predictions are added", {
+  local_dataset <- live::sample_locally(data = X,
+                                        explained_instance = X[3, ], 
+                                        explained_var = "V1",
+                                        size = 50)
+  local_dataset1 <- add_predictions(X, local_dataset, "regr.lm")
+  expect_output(add_predictions(X, local_dataset1, "regr.lm"), regexp = NA)
+  expect_equal(length(local_dataset1), 2)
+  expect_is(local_dataset1$data[[local_dataset1$target]], "numeric")
+  expect_is(local_dataset1$target, "character")
+  local_dataset2 <- add_predictions(X, local_dataset, lm(V1 ~., data = X))
+  expect_output(add_predictions(X, local_dataset, lm(V1 ~., data = X)), regexp = NA)
+  expect_equal(length(local_dataset2), 2)
+  expect_is(local_dataset2$data[[local_dataset2$target]], "numeric")
+  expect_is(local_dataset2$target, "character")
+})

ffab772ed62fd430e322c115811c20f5e6c5eb64
Mateusz Staniak <mateusz.staniak@math.uni.wroc.pl>
2018-05-03 23:56:36 +0200

    typos

--- R/live.R
+++ R/live.R
@@ -1,18 +1,18 @@
 #' live: visualizing interpretable models to explain black box models.
 #'
 #' This package aims to help locally fit and visualize interpretable models similarly to LIME methodology.
-#' Interface provided by mlr package is used. Tools are provided to create a simulated dataset of 
+#' Interface provided by mlr package is used. Tools are provided to create a simulated dataset of
 #' similar observations, fit a chosen white box models (GLM and CART in particular) and visualize
-#' them. 
+#' them.
 #'
-#' @section Important function:
+#' @section Important functions:
 #' \code{\link{sample_locally}} generates a dataset that will be used for local exploration.
-#' \code{\link{add_predictions}} add black box model predictions to simulated dataset.
+#' \code{\link{add_predictions}} adds black box model predictions to simulated dataset.
 #' \code{\link{fit_explanation}} fits a chosen white box model to simulated dataset.
 #' \code{\link{plot_explanation}} visualizes fitted model.
 #'
 #' @section Example datasets:
-#' \code{wine} Data on wine quality taken from 
+#' \code{wine} Data on wine quality taken from
 #' Modeling wine preferences by data mining from physicochemical properties
 #'
 #' @docType package
@@ -22,10 +22,10 @@ NULL
 #' Red wine characteristics and quality.
 #'
 #' Popular dataset related to wine samples from north Portugal.
-#'   
+#'
 #' @format Data frame with 1599 rows and 12 columns.
-#' 
-#' @references P. Cortez, A. Cerdeira, F. Almeida, T. Matos and J. Reis. 
+#'
+#' @references P. Cortez, A. Cerdeira, F. Almeida, T. Matos and J. Reis.
 #' Modeling wine preferences by data mining from physicochemical properties.
 #' In Decision Support Systems, Elsevier, 47(4):547-553, 2009.
 "wine"

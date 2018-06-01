ac1bed3cf7c9cd78bcac2cc60e13af7885c2a9e4
Mateusz Staniak <mateusz.staniak@math.uni.wroc.pl>
2018-06-01 22:18:17 +0200

    tests

--- tests/testthat/test_explaining.R
+++ tests/testthat/test_explaining.R
@@ -13,7 +13,7 @@ local_explained <- fit_explanation2(local1, "regr.lm")
 local_explained2 <- fit_explanation2(local1, "regr.ctree", kernel = identity_kernel)
 
 local2 <- sample_locally2(data = X2, explained_instance = X2[3, ],
-                         explained_var = "V1", size = 500)
+                         explained_var = "V1", size = 5000)
 local3 <- add_predictions2(local2, "classif.svm", X2)
 local_explained3 <- fit_explanation2(local3, "classif.logreg", predict_type = "prob")
 
@@ -26,6 +26,6 @@ test_that("Plots are created without problems", {
   expect_output(plot(local_explained, type = "waterfall"), regexp = NA)
   expect_output(plot(local_explained, type = "forest"), regexp = NA)
   expect_output(plot(local_explained2), regexp = NA)
-  expect_output(plot(local_explained3, type = "waterfall"))
+  expect_is(plot(local_explained3, type = "waterfall"), "ggplot")
 })
 

f64283187f22c6ca1b0bd3d2a1bb93d8e4ffd2a8
Mateusz Staniak <mateusz.staniak@math.uni.wroc.pl>
2018-07-18 12:21:40 +0200

    tests

--- tests/testthat/test_local_varimp.R
+++ tests/testthat/test_local_varimp.R
@@ -1,12 +1,20 @@
 context("Test local permutation importance")
 
 X_old
+X_old_long_names <- X_old
+colnames(X_old_long_names) <- paste0(colnames(X_old), "aaaaaaaa")
 lm_model <- lm(V1 ~., data = X_old)
+lm_model2 <- lm(V1aaaaaaaa ~., data = X_old_long_names)
 
 varimp <- local_permutation_importance(X_old[3, ], X_old, "V1", lm_model, 50)
+varimp2 <- local_permutation_importance(X_old_long_names[3, ], 
+                                        X_old_long_names, "V1aaaaaaaa",
+                                        lm_model2, 50)
+
 
 testthat::test_that("S3 method are okay", {
   testthat::expect_output(print(varimp))
+  testthat::expect_output(print(varimp2))
   testthat::expect_silent(plot(varimp))
 })
 
